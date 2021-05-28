7549a14e8b4688eb31f31d8f741601870b39f568
FarukIm-laptop <f.imamovic27@gmail.com>
2021-05-28 17:03:42 +0200

    imported csvs and added getname and list all names funnctions

--- project.py
+++ project.py
@@ -1,1 +1,14 @@
-import  csv
+import pandas as pd
+names = pd.read_csv('cityName.csv')
+edges = pd.read_csv('FromTo.csv')
+
+def getName(num):
+    return names.query(f'City_ID == {num}')['Name']
+
+def listNames(ids):
+    result = []
+    for i in ids:
+        result.append(getName(i))
+    return result
+
+print(listNames([1,2,3,4,5,6,10]))
