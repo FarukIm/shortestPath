e3de774b2a00f9b574311b5b69d8dc78e5fa9422
FarukIm <f.imamovic27@gmail.com>
2021-05-30 18:06:02 +0200

    added left node and right node lists

--- project.py
+++ project.py
@@ -3,22 +3,26 @@ names = pd.read_csv('cityName.csv')
 edges = pd.read_csv('FromTo.csv')
 left_node = []
 right_node = []
-for from_ID in edges:
-    left_node.append(edges[from_ID])
 
-print(left_node)
+for from_ID in edges['from_ID']:
+    left_node.append(from_ID)
 
-
-def getName(num):
+for to_ID in edges['to_ID']:
+    right_node.append(to_ID)
+     
+def get_name(num):
     return names.query(f'City_ID == {num}')['Name']
 
-def listNames(ids):
+def list_names(ids):
     result = []
     for i in ids:
-        result.append(getName(i))
+        result.append(get_name(i))
     return result
 
+def get_id(name):
+    return names.query(f'Name == {name}')['City_ID']
+
 def travel(a, b):
-    ids = []
-    count = 0
-   
+    start = get_id(a)
+    end = get_id(b)
+
